e70545c2ba13442a09adb3404bba0304c015ab1f
anagonm <anagonm@gmail.com>
2018-05-07 15:42:07 +0200

    Testing

--- index.test.js
+++ index.test.js
@@ -3,3 +3,14 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+describe("sum", () => {
+  it('adds positive and negative numbers', () => {
+    expect(sum(1, -2)).toBe(-1);
+    expect(sum(0,-2)).toBe(-2);
+  });
+  it('adds decimal numbers', () => {
+    expect(sum(1, 0.3)).toBe(1.3);
+    expect(sum(1, -0.3)).toBe(0.7);
+  });
+})
